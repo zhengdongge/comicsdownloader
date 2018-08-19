27e6ffcb2d58cd76d4484f837fc528d15aa36772
zhengdongge <banggzd@hotmail.com>
2018-08-19 15:27:20 -0400

    add page info

--- mh/mh/spiders/mh.py
+++ mh/mh/spiders/mh.py
@@ -8,8 +8,8 @@ from bs4 import BeautifulSoup
 import re
 
 #目前id可以从0001一直到6900 建议按照网站目录以**01-**00 100本为一次爬取目标
-start_id = 5501
-end_id = 5502
+start_id = 5501  # 4001 4071
+end_id = 5600  # 4100 4071
 
 class Comics(scrapy.Spider):
     name = "mh"
@@ -52,17 +52,19 @@ class Comics(scrapy.Spider):
                 url3 = pattern.search(str(script), re.I | re.U).group(3)
                 url4 = pattern.search(str(script), re.I | re.U).group(4)
                 url5 = pattern.search(str(script), re.I | re.U).group(5)
-        total_img = 600 # 假定一本漫画最多有600页
+        total_img = 600  # 假定一本漫画最多有600页
+        end_img = 0
         for img_num in range(1,total_img):
             img_url = url2 + url3 + url4 + url5 + str(img_num).zfill(3) + '.jpg'
             #self.log(img_url)
-            download = self.save_img(url_num, img_num, title, img_url, response)
+            download = self.save_img(url_num, img_num, title, img_url, end_img, response)
             # 返回值为false表示已经产生404 爬完了该漫画的所有图片了
             if not download:
                 end_img = img_num - 1
+                self.save_img(url_num, img_num, title, img_url, end_img, response)  # 修改文件夹名
                 break
 
-    def save_img(self, url_num, img_num, title, img_url, response):
+    def save_img(self, url_num, img_num, title, img_url, end_img, response):
         # 将图片保存到本地
         self.log('saving pic: ' + img_url)
 
@@ -74,6 +76,11 @@ class Comics(scrapy.Spider):
         title = url_num + '_' + title
         comics_path = document + '/' + title
         # 创建新的目录
+        os.chdir(document)
+        for folder in os.listdir(document):
+            if title in folder:
+                comics_path = os.path.relpath(folder, start=document)
+                break
         exists = os.path.exists(comics_path)
         if not exists:
             self.log('create document: ' + title)
@@ -85,6 +92,13 @@ class Comics(scrapy.Spider):
             fobj.write(response.request.url + '\n')  # 保存漫画页面url
             fobj.write(img_url + '\n')  # 保存漫画图库第一张照片url
             fobj.close()
+
+        # 爬完一本漫画后且未修改过漫画文件夹名 修改文件夹名 添加页数信息
+        if (end_img != 0) and (folder == title):
+            comics_path_old = comics_path
+            comics_path_new = comics_path + '[' + str(end_img).zfill(3) + 'p]'
+            os.rename(comics_path_old, comics_path_new)
+            return
 
         # 每张图片以页数命名
         pic_name = comics_path + '/' + str(img_num).zfill(3) + '.jpg'
